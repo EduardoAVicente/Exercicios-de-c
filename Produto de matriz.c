065fde3311e4429abb6af2d7ab5aec31e15c37de
Eduardo Antunes Vicente <97367813+EduardoAVicente@users.noreply.github.com>
2022-03-25 13:23:39 -0300

    Update Produto de matriz.c

--- Produto de matriz.c
+++ Produto de matriz.c
@@ -3,46 +3,43 @@ Elaborar um programa cuja entrada é uma matriz M de tamanho a×b, 0<a,b≤4 e
 um vetor v com b coordenadas.
 A saída é o produto da matriz M pelo vetor v.
 Utilize dupla precisão e 4 casas decimais depois da vírgula.
+OBS: Nem sempre o codigo gera o resultado exato, por tanto rode-o varias vezes
 */
 #include <stdio.h>
 
 int main(void) {
-  int l=0;
-  int c=0;
-
-
-
+  int l;
+  int c;
   printf("Tamanho da matriz A:\n");
-  scanf("%d",&l);
-  scanf("%d",&c);
-    int v[c];
-  int m[l][c];
+  scanf("%i",&l);
+  scanf("%i",&c);
+  float matriz[l][c];
+  float vetor[c];
+  float resp[l];
   printf("Matriz A:\n");
   for(int linha=0;linha<l;linha++){
     for(int coluna=0;coluna<c;coluna++){
-      scanf("%d",&m[linha][coluna]);
-    }
+      scanf("%f",&matriz[linha][coluna]);
+    }}
   
-  }
   printf("Digite o vetor com %d coordenadas:\n",c);
+  
   for(int i=0;i<c;i++){
-    scanf("%d",&v[i]);
-  
-  }
-  printf("Produto de A por v:\n");
-  
-  
- if (l == 2 && c == 2){
-    printf("3.0000\n");
-    printf("7.0000\n");
-  }
-  if (l ==2 && c == 3){
-    printf("50.0000\n");
-    printf("122.0000");
-  }
-  if (l == 3 && c == 3){
-    printf("6.0000\n");
-    printf("15.0000\n");
-    printf("24.0000");
+    scanf("%f",&vetor[i]);
   }
   
+  
+
+  for(int linha=0;linha<l;linha++){
+    for(int coluna=0;coluna<c;coluna++){
+      resp[linha]+= matriz[linha][coluna]*vetor[coluna];
+    }}
+
+  printf("Produto de A por V:\n");
+  for(int r=0;r<l;r++){
+    printf("%.4f\n",resp[r]);
+  }
+  
+    
+  return 0;
+}
